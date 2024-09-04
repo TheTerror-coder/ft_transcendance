39cfb09eb508eec83ed693bc22cc15729ae7637c
TheTerror <johnmrfaye12@gmail.com>
2024-09-04 18:53:58 +0200

    merge from dbaule to premaster

---
+++ sources/flag.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+englandFlag.onclick = translateEnglish;
+spainFlag.onclick = translateSpanish;
+franceFlag.onclick = translateFrench;
+
+
+function translateEnglish()
+{
+    if (createAccountForm)
+        createAccountChange.innerHTML = "";
+    if (flagSelected != "en")
+    {
+        const stringConnexion = `CONNECTION API <img src="picturePng/logo42.png" alt="42-logo" style="margin-top:-10px;">`
+        buttonConnectionAPI42.innerHTML = stringConnexion;
+        buttonConnecEmail.innerHTML = "CONNECTION E-MAIL";
+        buttonCreateAccount.innerHTML = "CREATE AN ACCOUNT";
+        flagSelected = "en";
+    }
+}
+function translateSpanish()
+{
+    if (createAccountForm)
+        createAccountChange.innerHTML = "";
+    if (flagSelected != "es")
+    {
+        const stringConnexion = `CONEXION API <img src="picturePng/logo42.png" alt="42-logo" style="margin-top:-10px;">`
+        buttonConnectionAPI42.innerHTML = stringConnexion;
+        buttonConnecEmail.innerHTML = "CONEXION E-MAIL";
+        buttonCreateAccount.innerHTML = "CREAR UNA CUENTA";
+        flagSelected = "es";
+    }
+}
+
+function translateFrench()
+{
+    if (createAccountForm)
+        createAccountChange.innerHTML = "";
+    if (flagSelected != "fr")
+    {
+        const stringConnexion = `CONNEXION API <img src="picturePng/logo42.png" alt="42-logo" style="margin-top:-10px;">`
+        buttonConnectionAPI42.innerHTML = stringConnexion;
+        buttonConnecEmail.innerHTML = "CONNEXION E-MAIL";
+        buttonCreateAccount.innerHTML = "CREER UN COMPTE";
+        flagSelected = "fr";
+    }
+}
