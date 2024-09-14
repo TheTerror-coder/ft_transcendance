68d8114569b0552c7cdc9bff3579e2bd240ea897
theterror-coder <jfaye@student.42lyon.fr>
2024-09-14 22:07:42 +0200

    upgrade: setup script refinement

--- sources/srcs/requirements/elk/setup/tools/script.sh
+++ sources/srcs/requirements/elk/setup/tools/script.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
-bash -c '
+essentials_checking() {
 	if [ x$(cat $ELASTIC_PASSWORD_FILE) == x ]; then
 		echo "Set the ELASTIC_PASSWORD_FILE environment variable in the .env file";
 		exit 1;
@@ -8,6 +8,9 @@ bash -c '
 		echo "Set the KIBANA_PASSWORD_FILE environment variable in the .env file";
 		exit 1;
 	fi;
+}
+
+create_certs() {
 	if [ ! -f config/certs/ca.zip ]; then
 		echo "Creating CA";
 		mkdir -p /usr/share/elasticsearch/config/certs
@@ -39,20 +42,25 @@ bash -c '
 		> config/certs/instances.yml;
 		bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
 		unzip config/certs/certs.zip -d config/certs;
-		openssl pkcs8 -inform PEM -in config/certs/elasticsearch/elasticsearch.key -topk8 -nocrypt -outform PEM -out config/certs/elasticsearch/elasticsearch.pkcs8.key
 	fi;
+}
 
-	echo "Setting file permissions"
+permissions() {
+	echo "Setting file/directory permissions"
 	chown -R root:root config/certs;
 	find . -type d -exec chmod 750 \{\} \;;
 	find . -type f -exec chmod 640 \{\} \;;
+}
 
+wait_elastic() {
 	echo "Waiting for Elasticsearch availability";
 	until curl -s --cacert config/certs/ca/ca.crt ${ELASTICSEARCH_HOSTXPORT} | grep -q "missing authentication credentials"; do sleep 30; done;
 	
 	echo "Setting ${KIBANA_USER} password";
 	until curl -s -X POST --cacert config/certs/ca/ca.crt -u "${ELASTIC_USER}:$(cat $ELASTIC_PASSWORD_FILE)" -H "Content-Type: application/json" ${ELASTICSEARCH_HOSTXPORT}/_security/user/${KIBANA_USER}/_password -d "{\"password\":\"$(cat $KIBANA_PASSWORD_FILE)\"}" | grep -q "^{}"; do sleep 10; done;
-	
+}
+
+logstash_user() {
 	echo "creating logstash_writer role";
 	curl -s -X POST --cacert config/certs/ca/ca.crt \
 		-u "${ELASTIC_USER}:$(cat $ELASTIC_PASSWORD_FILE)" \
@@ -78,6 +86,11 @@ bash -c '
 			\"roles\" : [ \"${LOGSTASH_ES_ROLE}\" ], \
 			\"full_name\" : \"${LOGSTASH_ES_USERFULLNAME}\" \
 		}"
+}
 
-	echo "All done!";
-'
+essentials_checking
+create_certs
+permissions
+wait_elastic
+logstash_user
+echo "All done!";
