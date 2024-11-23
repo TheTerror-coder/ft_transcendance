ca33698d03d97e1b787373a415c7395d1bbfd732
Nicolas Bernard <nibernar@z3r6p4.42lyon.fr>
2024-11-23 13:40:06 +0100

    commit to pull spaRouting

--- sources/srcs/requirements/backend/srcs/backend/asgi.py
+++ sources/srcs/requirements/backend/srcs/backend/asgi.py
@@ -5,9 +5,7 @@ from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from .routing import websocket_urlpatterns
 from usermanagement.consumers import FriendInviteConsumer
-# from usermanagement.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
@@ -17,7 +15,6 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            # path('websocket/friend_invite/', ChatConsumer.as_asgi()),
             path("websocket/friend_invite/", FriendInviteConsumer.as_asgi()),
         ])
     ),
