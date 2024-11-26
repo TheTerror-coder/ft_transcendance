4d145d6b35cf1978f2fcc12189902e4e4f063793
theterror-coder <jfaye@student.42lyon.fr>
2024-11-26 22:27:25 +0100

    fix&feature: social account  jwt was not ok: after first auth at local user account creation, the jwt was not generated with the manner i was managing the generation (signals). Now i generate the token just after local account is created (in save_user function)

--- sources/srcs/requirements/backend/srcs/oauth/signals.py
+++ sources/srcs/requirements/backend/srcs/oauth/signals.py
@@ -4,21 +4,25 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .tokens import CustomRefreshToken
 
-@receiver(social_account_added)
 @receiver(social_account_updated)
 def issue_jwt_token(sender, request, sociallogin, **kwargs):
 	"""
 	This signal is fired when a social account is updated or logged in.
 	We use it to issue a JWT token for the authenticated user.
 	"""
-	sys.stderr.write("*******DEBUG******* display: " + sociallogin.user.username + '\n')
-	user = sociallogin.user
-	refresh = CustomRefreshToken.for_user(user)
-	access = str(refresh.access_token)
-	jwt_access_token = str(access)
-	jwt_refresh_token = str(refresh)
-	sys.stderr.write("*******DEBUG******* refresh token: " + jwt_refresh_token + '\n')
-	sys.stderr.write("*******DEBUG******* access token: " + jwt_access_token + '\n')
 	
-	request.session["jwt_access_token"] = jwt_access_token
-	request.session["jwt_refresh_token"] = jwt_refresh_token
+	sys.stderr.write("*******DEBUG******* issue_jwt_token() display: " + (sociallogin.user.username or 'null') + '\n')
+	user = sociallogin.user
+	
+	if user:
+		refresh = CustomRefreshToken.for_user(user)
+		access = str(refresh.access_token)
+		if refresh and access:
+			jwt_access_token = str(access)
+			jwt_refresh_token = str(refresh)
+
+			sys.stderr.write("*******DEBUG******* issue_jwt_token() refresh token: " + jwt_refresh_token + '\n')
+			sys.stderr.write("*******DEBUG******* issue_jwt_token() access token: " + jwt_access_token + '\n')
+	
+			request.session["jwt_access_token"] = jwt_access_token
+			request.session["jwt_refresh_token"] = jwt_refresh_token
