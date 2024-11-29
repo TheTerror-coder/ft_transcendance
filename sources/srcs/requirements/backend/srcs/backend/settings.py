ab06b820c8b9ba81cea9fda0456c0cd29c45745e
theterror-coder <jfaye@student.42lyon.fr>
2024-11-29 18:41:22 +0100

    fix: USER SESSION: user session not working when jwtauthentication is enabled in settings.py

--- sources/srcs/requirements/backend/srcs/backend/settings.py
+++ sources/srcs/requirements/backend/srcs/backend/settings.py
@@ -231,14 +231,14 @@ CHANNEL_LAYERS = {
     },
 }
 
-REST_FRAMEWORK = {
-	'DEFAULT_AUTHENTICATION_CLASSES': (
-		'rest_framework_simplejwt.authentication.JWTAuthentication',
-	),
-	# 'DEFAULT_PERMISSION_CLASSES': [
-	# 	'rest_framework.permissions.IsAuthenticated',
-	# ],
-}
+# REST_FRAMEWORK = {
+# 	'DEFAULT_AUTHENTICATION_CLASSES': (
+# 		'rest_framework_simplejwt.authentication.JWTAuthentication',
+# 	),
+# 	# 'DEFAULT_PERMISSION_CLASSES': [
+# 	# 	'rest_framework.permissions.IsAuthenticated',
+# 	# ],
+# }
 SIMPLE_JWT = {
 	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
