fbe0fc5e2c265ffa0cc1615590fecb18c7eaaeff
theterror-coder <jfaye@student.42lyon.fr>
2025-01-03 19:34:36 +0100

    fix: host ip fix

--- sources/srcs/tools/host-ip.sh
+++ sources/srcs/tools/host-ip.sh
@@ -1,3 +1,16 @@
 #!/bin/sh -e
 
+if ifconfig | grep -q wlp; then
+	HOST_IP=$(ifconfig | grep -A 3 wlp | grep 'inet ' | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
+elif ifconfig | grep -q enp; then
+	HOST_IP=$(ifconfig | grep -A 3 enp | grep 'inet ' | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
+else
+    # "No suitable network interface found"
+	exit 1
+fi
+
 sed -i "s/^\(_HOST_IP_[[:space:]]*=\).*/\1$HOST_IP/" $1
+
+echo "$HOST_IP"
+
+exit 0
